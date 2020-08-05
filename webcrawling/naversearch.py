ec80f07bb4665c1ea35f23c05fe440b8f341ab93
kbr2788 <kbr2788@naver.com>
2020-08-05 19:30:22 +0900

    Update naversearch.py
    2nd

--- webcrawling/naversearch.py
+++ webcrawling/naversearch.py
@@ -2,6 +2,8 @@ import requests
 
 url = 'https://apis.naver.com/mobile_main/srchrank/srchrank?frm=main&ag=20s&gr=1&ma=-1&si=-1&en=-1&sp=-1'
 
+#requests 보내기 
 response = requests.get(url)
 
+#데이터 확인
 response.text
