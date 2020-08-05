682338900f972481f4f68ac94bb270e3c663a1dc
kbr2788 <kbr2788@naver.com>
2020-08-05 19:53:27 +0900

    Update naversearch.py
    fin

--- webcrawling/naversearch.py
+++ webcrawling/naversearch.py
@@ -1,5 +1,6 @@
 import requests
 
+#해당 url 준비하기
 url = 'https://apis.naver.com/mobile_main/srchrank/srchrank?frm=main&ag=20s&gr=1&ma=-1&si=-1&en=-1&sp=-1'
 
 #requests 보내기 
@@ -7,3 +8,17 @@ response = requests.get(url)
 
 #데이터 확인
 response.text
+
+#python 데이터로 변환하기 (list,dict)
+data = response.json()
+
+#데이터 확인
+#data
+
+#원하는 데이터 추출하기
+rank_data = data['data']
+
+[
+    str(i['rank'])+ '. ' + i['keyword'] 
+    for i in rank_data
+]
